129b17445e6da14ae8a489f39a78dbfb036c7fb8
Andrew Trent <andrewtrentdesign@gmail.com>
2018-11-06 17:53:12 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -36,7 +36,7 @@ class Artist
 
   def self.find_or_create_by_name(artist_name)
 
-    if self.find_by_name(artist_name)
+    if self.find_by_name(artist_name) != nil
       self.find_by_name(artist_name)
     else
       self.new(artist_name)
